1432abbc495828de23a6625683f11cc301c21423
jktitareva <julia.ktitareva@mvise.de>
2018-11-25 18:34:18 +0100

    json imports und maven dependencies, aus json verzweiungen auslesen klappt bisher noch nicht

--- src/main/java/soupit/Test.java
+++ src/main/java/soupit/Test.java
@@ -1,17 +1,58 @@
 package main.java.soupit;
 
-import main.java.soupit.HilfsKlassen.Rezepte.KartoffelcremeSuppe;
 import main.java.soupit.HilfsKlassen.Rezept;
 import main.java.soupit.HilfsKlassen.RezeptArrayList;
+import main.java.soupit.HilfsKlassen.Rezepte.KartoffelcremeSuppe;
 import main.java.soupit.HilfsKlassen.Zutat;
 import main.java.soupit.HilfsKlassen.ZutatMengeEinheit;
+import java.io.FileReader;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
 
 
 public class Test {
     public static final RezeptArrayList REZEPT_ARRAY_LIST = new RezeptArrayList(new KartoffelcremeSuppe());
 
 
-    public static void main(String... args) {
+    public static void main(String... args) throws Exception{
+
+        Object obj = new JSONParser().parse(new FileReader("C:/Users/jktitareva/Documents/soupit/skillproject-fr-22/data/rezepte.json"));
+
+        // typecasting obj to JSONObject
+        JSONObject jo = (JSONObject) obj;
+
+
+        // getting firstName and lastName
+        String firstName = (String) jo.get("hey");
+
+        System.out.println(firstName);
+
+
+        // getting phoneNumbers
+        JSONArray ja = (JSONArray) jo.get("zutat");
+
+        // iterating phoneNumbers
+        Iterator itr2 = ja.iterator();
+
+        while (itr2.hasNext())
+        {
+            Iterator itr1 = ((Map) itr2.next()).entrySet().iterator();
+            while (itr1.hasNext()) {
+                Map.Entry pair = (Map.Entry) itr1.next();
+                System.out.println(pair.getKey() + " : " + pair.getValue());
+            }
+        }
+
+
+
+
+
+
         String speechText;
         String[] strinGredients = {"kartoffeln"};
         Zutat[] ingredients = new Zutat[strinGredients.length];
