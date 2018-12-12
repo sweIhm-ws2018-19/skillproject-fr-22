813f5a56077415e7b3d05d4471ecd93802ac3f39
Zzerr0r <43878904+Zzerr0r@users.noreply.github.com>
2018-12-12 13:48:18 +0100

    YesNoIntent template added

--- src/main/java/soupit/handlers/YesNoIntent.java
+++ src/main/java/soupit/handlers/YesNoIntent.java
@@ -3,6 +3,7 @@ package soupit.handlers;
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.model.*;
+import soupit.Lists.Strings;
 import soupit.SoupITStreamHandler;
 
 import java.util.Map;
@@ -10,10 +11,10 @@ import java.util.Optional;
 
 import static com.amazon.ask.request.Predicates.intentName;
 
-public class RecipeYesOrNoIntent implements RequestHandler{
+public class YesNoIntent implements RequestHandler{
 
         public boolean canHandle(HandlerInput input) {
-            return input.matches(intentName("RecipeYesOrNoIntent"));
+            return input.matches(intentName("YesNoIntent"));
         }
 
         @Override
@@ -24,6 +25,11 @@ public class RecipeYesOrNoIntent implements RequestHandler{
             Map<String, Slot> slots = intent.getSlots();
             Slot yesNoSlot = slots.get("YesNo");
             String speechText;
+            if(SoupITStreamHandler.ProgramState.equals(Strings.SOUP_YES_NO_STATE)){
+                //Cooker.cook(MomentarySoup)
+            }else if(SoupITStreamHandler.ProgramState.equals(Strings.INGREDIENTSAVAILIABLE)){
+                //do something esle
+            }
             if(yesNoSlot != null){
                 speechText = "du hast dich für "+ SoupITStreamHandler.recipeToDecideOn +" entschieden";
             }else{
